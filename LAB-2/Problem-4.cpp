23e909a5a0e436f2942e79c6bad5d6ab2d6919f9
4canzz <4canzz@gmail.com>
2024-10-26 00:45:34 +0500

    compiling first 3 labs

--- LAB-2/Problem-4.cpp
+++ LAB-2/Problem-4.cpp
@@ -1,3 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    float celsius;
+    float F;
+    cout << "enter value of temprature in celsius" << endl;
+    cin >> celsius;
+    F = (celsius * 9/5)+32;
+    cout << "the value in Farenheit is: " << endl;
+
+}
+
 /*You can convert temperature from degrees Celsius to degrees Fahrenheit by multiplying by 9/5
 and adding 32. Write a C++ program that allows the user to enter a floating-point number
 representing degrees Celsius, and then displays the corresponding degrees Fahrenheit.
