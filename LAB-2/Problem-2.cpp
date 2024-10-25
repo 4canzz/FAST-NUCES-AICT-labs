b68d6526004f9bcdc3fe532b9cc1286d1e2a9b68
4canzz <4canzz@gmail.com>
2024-10-26 00:41:09 +0500

    semi colon error oops

--- LAB-2/Problem-2.cpp
+++ LAB-2/Problem-2.cpp
@@ -4,11 +4,11 @@ using namespace std;
 
 int main(){
     int number = 12345;
-    int tt = (number % 10)*10000
-    int thousands = (number / 10 % 10)*1000
-    int huns = (number / 100 % 10 )*100
-    int tens = (number / 1000 % 10)*10
-    int ones = number /10
+    int tt = (number % 10)*10000;
+    int thousands = (number / 10 % 10)*1000;
+    int huns = (number / 100 % 10 )*100;
+    int tens = (number / 1000 % 10)*10;
+    int ones = number /10;
     cout << tt + thousands + huns + tens + ones << endl;
     return 0;
 }
