42adc5ee073eb7f877247720acf298cb8ec8332c
4canzz <4canzz@gmail.com>
2024-10-30 23:54:57 +0500

    compiled all 3 problem sets for lab 5; QA soon

--- LAB-5/Problem-3.cpp
+++ LAB-5/Problem-3.cpp
@@ -11,40 +11,40 @@ int main()
    
     switch (marks/10){
         case 10:
-case 9:
+        case 9:
             cout << "Grade A" << endl;
             break;
-case 8:
-case 7:
-switch(marks>=75){
-case true:
-cout << "Grade B" << endl;
-break;
-case false:
-cout << " ";
-}
-case 6:
-cout << "Grade C" << endl;
-            break;
-case 5:
-case 4:
-switch(marks>=45){
-case true:
-cout << "Grade D" << endl;
-break;
-case false:
-cout << " ";
-}
-case 3:
-case 2:
-case 1:
-case 0:
-cout << "Grade F" << endl;
-            break;
-}    
-system ("pause");
+        case 8:
+        case 7:
+            switch(marks>=75){
+                case true:
+                    cout << "Grade B" << endl;
+                    break;
+                case false: 
+                    cout << " ";    
+            }
+    case 6:
+        cout << "Grade C" << endl;
+        break;
+    case 5:
+    case 4:
+        switch(marks>=45){  
+            case true:
+                cout << "Grade D" << endl;
+                break;
+            case false:
+                cout << " ";
+        }
+    case 3:
+    case 2:
+    case 1:
+    case 0:
+        cout << "Grade F" << endl;
+        break;
+    }  
     return 0;
-}/*Exercise - 3
+}
+/*Exercise - 3
 Write a program in C++ using SWITCH CASE operator to find the grade of a student.
 The details are as follow
 marks >= 90 Grade A
