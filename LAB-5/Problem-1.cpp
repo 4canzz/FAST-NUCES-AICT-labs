57bb5f02a7d520832bf12384f4f3bb2956d0c23a
4canzz <4canzz@gmail.com>
2024-10-30 23:41:48 +0500

    working on LAB 5, edit lab 4 problem 2 comment

--- LAB-5/Problem-1.cpp
+++ LAB-5/Problem-1.cpp
@@ -5,19 +5,21 @@ int year;
 int main(){
     cout << "enter year: ";
     cin >> year;
-    if((year % 4 == 0 && year % 100 != 0) || (year % 100 == 0 && year % 400 == 0)){
-        cout << "it is a leap year" << endl;
-    }
-    else{
-        cout << "it is not a leap year" << endl;
-    }
-    return 0;
+    switch((year % 4 == 0 && year % 100 != 0) || (year % 100 == 0 && year % 400 == 0)){
+        case true:
+            cout << "it is a leap year" << endl;
+            break;
+        default:
+            cout << "it is not a leap year" << endl;
+            break;
+}
+return 0;
 }
 /*Any year is input by the user. Write a program to determine whether the year is a leap year or
 not.
 How to determine whether a year is a leap year
 To determine whether a year is a leap year, follow these steps:
-
+USE SWITCH CASE and not IF ELSE
 1. If the year is evenly divisible by 4, go to step 2. Otherwise, go to step 5.
 2. If the year is evenly divisible by 100, go to step 3. Otherwise, go to step 4.
 3. If the year is evenly divisible by 400, go to step 4. Otherwise, go to step 5.
