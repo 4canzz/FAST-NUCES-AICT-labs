928fad32cb404b0627e3c63f51a94818eb15c03a
4canzz <4canzz@gmail.com>
2024-10-30 23:45:04 +0500

    lab 5 problem 2 problem added

---
+++ LAB-5/Problem-2.cpp
@@ -0,0 +1,9 @@
+/*Exercise - 2
+Write a C++ to perform arithmetic Operations using switch case.
+1. Read two numbers ‘a’ and ‘b’ and &#39;Read your choice of operator ch.
+2. If ch = ‘+’ then calculate add = a + b and display the addition result.
+3. If ch= ‘- ‘then calculate sub = a – b and display the subtraction result.
+4. If ch= ‘*’ then calculate mul = a * b and display the multiplication result.
+5. If ch= ‘/’ then calculate div = a / b and display the division result.
+6. If ch=’%’ then calculate mod = a % b and display the modulus result.
+7. Otherwise display invalid operator.*/
