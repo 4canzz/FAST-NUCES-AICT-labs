d57c3e52f32f5ce388658873b516089d2283430d
4canzz <4canzz@gmail.com>
2024-10-30 23:46:33 +0500

    modified:   LAB-5/Problem-3.cpp

---
+++ LAB-5/Problem-3.cpp
@@ -0,0 +1,8 @@
+/*Exercise - 3
+Write a program in C++ using if else operator to find the grade of a student.
+The details are as follow
+marks &gt;= 90 Grade A
+marks &gt;= 75 Grade B
+marks &gt;= 60 Grade C
+marks &gt;= 45 Grade D
+else Grade F*/
