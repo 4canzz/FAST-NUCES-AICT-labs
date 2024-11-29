f2edac42c8b2a7d539cd039918cc374bd6daf330
4canzz <4canzz@gmail.com>
2024-11-29 20:20:26 +0500

    added Q6

---
+++ Assignment-3/Q6.cpp
@@ -0,0 +1,22 @@
+/*Question No. 6 Marks: 10
+Construct a recursive function to calculate the factorial of a given number n. Also write main function to
+check its functionality.*/
+
+#include <iostream>
+using namespace std; 
+
+int factorial (int n);
+
+int main() {
+    int n;
+    cout << "enter digit: ";
+    cin >> n;
+    cout << factorial(n) << endl;
+}
+
+int factorial(int n) {
+    if (n == 0) {
+        return 1;
+    }
+    return n * factorial(n - 1);
+}
