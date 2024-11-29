7c98623c81c4b3b30c66906484234d111c19754a
4canzz <156192043+4canzz@users.noreply.github.com>
2024-11-29 19:09:26 +0500

    Update Q1.cpp

--- Assignment-2/Q1.cpp
+++ Assignment-2/Q1.cpp
@@ -1,1 +1,21 @@
+/*Question No 1. Write a C++ program to print the Fibonacci sequence up to a given
+number using a while loop. Explain why is a while loop suitable in this scenario?*/
+#include <iostream>
+using namespace std;
 
+int fib_seq_val = 0, num1 = 0, num2 = 1, steps = 0;
+
+int main() {  
+  while (steps <= 10) {
+    cout << fib_seq_val << " ";
+    num1 = fib_seq_val + num2;
+    fib_seq_val = num2;
+    num2 = num1;
+    steps++;
+  }
+  return 0;
+}
+/*Using a while loop is necessary because the looping is indeterminate, no one knows in advance
+how many iterations the loop will contain. This construct also enhances readability, because
+from this structure it is simple to understand that you are generating the nth Fibonacci number
+continuously up to the point n.*/
