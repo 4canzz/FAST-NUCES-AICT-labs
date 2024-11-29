bacff093e1032ae951a9df5210aef9588435645b
4canzz <4canzz@gmail.com>
2024-11-29 20:27:42 +0500

    added Q10

---
+++ Assignment-3/Q10.cpp
@@ -0,0 +1,25 @@
+/*Question No. 10 Marks: 10
+Implement a recursive function to calculate the GCD of two numbers using the Euclidean algorithm.*/
+#include <iostream>
+using namespace std;
+
+int hcf(int num1, int num2);
+
+int main() {
+    int num1, num2;
+    cout << "Enter two positive integers: ";
+    cin >> num1 >> num2;
+    while(num1 < 0 || num2 < 0) {
+        cout << "pleae enter valid input (positive integer): ";
+        cin >> num1 >> num2;
+    }
+    return 0;
+}
+
+int hcf(int num1, int num2) {
+    if (num2 != 0)
+        return hcf(num2, num1 % num2);
+    else {
+        return num1;
+    }
+}
